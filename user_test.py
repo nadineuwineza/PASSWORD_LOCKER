07e0e4191ead80f55c9a9512b9e0942d68039747
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-22 20:34:01 +0200

    creating a file README

--- user_test.py
+++ user_test.py
@@ -12,3 +12,6 @@ class TestUser(unittest.TestCase):
     def test_login(self):
         self.assertTrue(self.new_user.username, 'Nadine')
         self.assertTrue(self.new_user.password, 'nana')    
+
+if __name__ == '__main__':
+    unittest.main()        
