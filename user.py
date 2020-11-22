50bade74b5e2407659a6d178e00a62da95486941
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-22 19:19:00 +0200

    add method to initialise credentials

--- user.py
+++ user.py
@@ -1,3 +1,21 @@
 import random
 import string
 
+
+users = list()
+credentials = list()
+
+"""
+declared two empty lists to store data
+"""
+
+class Credentials:
+    
+    def __init__(self, account_name, username, password):
+        self.account_name = account_name
+        self.username = username
+        self.password = password
+
+        """
+        Created the class credentials and initialized using the self keyword
+        """
