ae48df6986e559477eaa3b95f356c82f228e806e
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-22 19:24:10 +0200

    add method to create store and view credentials

--- user.py
+++ user.py
@@ -19,3 +19,13 @@ class Credentials:
         """
         Created the class credentials and initialized using the self keyword
         """
+    def my_credentials():
+        print("Choose one of the following options to continue")
+        print("\n1. Create new credentials \n2. Store credentials of existing accounts \n3. View existing credentials \n4. Logout")
+        credentials_option = input()
+        if credentials_option == "1":
+            print("Do you want a system generated password?")
+            option = input("\n1. Yes \n2. No")
+            if option == "1":
+                pwd =Users.random_password(10)
+                acc
